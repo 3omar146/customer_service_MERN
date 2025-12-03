a45557e5574efd242ad4ee637ce843da751d6b74
Omar Tamer <omardaihom@gmail.com>
2025-12-03 11:16:39 +0200

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -5,11 +5,13 @@ import cors from 'cors';
 import supervisorRouter from './Routers/SupervisorRouter.js';
 import agentRouter from './Routers/AgentRouter.js';
 import caseRouter from './Routers/CaseRouter.js';
+
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
+
 //mongo db connection
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("Connected to MongoDB"))
@@ -25,8 +27,6 @@ app.use('/supervisors', supervisorRouter);
 app.use('/agents', agentRouter);
 //case routes
 app.use('/cases', caseRouter);
-
-
 
 
 
