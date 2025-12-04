5440030b1b8711c206c177ee050eef37af1d995e
Adam <23P0001@eng.asu.edu.eg>
2025-12-04 20:02:14 +0200

    Update App.jsx

--- Frontend/customer-service/src/App.jsx
+++ Frontend/customer-service/src/App.jsx
@@ -2,14 +2,17 @@
 import './App.css'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import AgentDashboard from './pages/AgentDashboard.jsx';
+import ClientDashboard from './pages/ClientDashboard.jsx';
 import SupervisorTable from './Components/SupervisorTable.jsx';
 import AgentDetails from './pages/AgentDetails.jsx';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/agentDetail" element={<AgentDetails AgentID={"692f59c9a33b1df1ddaff013"} />} />
         <Route path="/SupervisorTable" element={<SupervisorTable supervisorID={"6931a93378ece62bd5dbbc95"} />} />
+        <Route path="/client-dashboard" element={<ClientDashboard supervisorID={"6911a93378ece62bd5dbbc95"} />} />
       </Routes>
     </Router>
   );
